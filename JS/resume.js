147bc3993283db1ac9f0cc63a15c06917cb49f99
Austin McClendon <155007742+AustinMccHTML@users.noreply.github.com>
2024-01-05 11:00:38 -0700

    v2

---
+++ JS/resume.js
@@ -0,0 +1,17 @@
+document.addEventListener("DOMContentLoaded", function() {
+    var links = document.querySelectorAll('a[href^="#"]');
+    
+    links.forEach(function(link) {
+        link.addEventListener("click", function(event) {
+            event.preventDefault();
+
+            var targetId = this.getAttribute("href").substring(1);
+            var targetElement = document.getElementById(targetId);
+
+            window.scrollTo({
+                top: targetElement.offsetTop - header.offsetHeight,
+                behavior: "smooth"
+            });
+        });
+    });
+});
